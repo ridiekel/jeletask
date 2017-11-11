fc2d55403c61ab20e8596d73932333986ebf0521
Geroen Dierckx <geroen.dierckx@gmail.com>
2017-11-11 12:45:22 +0100

    Removed old webapp

--- webapps/rest/src/main/java/be/xhibit/teletask/webapp/rest/ota/TimedOneTimeAccessToken.java
+++
@@ -1,8 +0,0 @@
-package be.xhibit.teletask.webapp.rest.ota;
-
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
-@JsonSerialize(as = TimedOneTimeAccessToken.class)
-public interface TimedOneTimeAccessToken extends OneTimeAccessToken {
-    String getValidTime();
-}
