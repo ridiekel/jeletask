79ca7e259664557a86b4ddde5f5d0e4de6768d96
SanderM2 <andy@knuts.be>
2022-09-13 21:34:27 +0200

    Update HAMotorConfig.java

--- jeletask2mqtt/src/main/java/io/github/ridiekel/jeletask/mqtt/listener/homeassistant/types/HAMotorConfig.java
+++ jeletask2mqtt/src/main/java/io/github/ridiekel/jeletask/mqtt/listener/homeassistant/types/HAMotorConfig.java
@@ -6,10 +6,15 @@ import io.github.ridiekel.jeletask.mqtt.listener.homeassistant.HAReadWriteConfig
 public class HAMotorConfig extends HAReadWriteConfig<HAMotorConfig> {
     public HAMotorConfig(HAConfigParameters parameters) {
         super(parameters);
-
-//        this.putInt("brightness_scale", 100);
-//        this.put("on_command_type","brightness");
-//        this.put("brightness_command_topic", "~/set");
-
+        this.put("position_topic", "~/state");
+        this.put("set_position_topic", "~/set");
+        this.put("command_topic", "~/set");
+        this.putInt("position_open", 0);
+        this.putInt("position_closed", 100);
+        this.put("optimistic", "false");
+        this.put("payload_open", "UP");
+        this.put("payload_close", "DOWN");
+        this.put("payload_stop", "STOP");
+        this.put("position_template", "{{ value_json.position }}");
     }
 }
