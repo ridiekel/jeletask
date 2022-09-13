18eef1118beb357254f79545ef201717fb7dcf4a
SanderM2 <andy@knuts.be>
2022-09-13 21:14:17 +0200

    Update HARelayConfig.java

--- jeletask2mqtt/src/main/java/io/github/ridiekel/jeletask/mqtt/listener/homeassistant/HARelayConfig.java
+++ jeletask2mqtt/src/main/java/io/github/ridiekel/jeletask/mqtt/listener/homeassistant/HARelayConfig.java
@@ -3,8 +3,7 @@ package io.github.ridiekel.jeletask.mqtt.listener.homeassistant;
 public class HARelayConfig extends HAReadWriteConfig<HARelayConfig> {
     public HARelayConfig(HAConfigParameters parameters) {
         super(parameters);
-        String type = parameters.getComponentSpec().getType();
-        switch ( type.toLowerCase() ) {
+        switch ( parameters.getComponentSpec().getType().toLowerCase() ) {
             case "light":
                 this.put("state_value_template", "{{ value_json.state }}");
                 break;
