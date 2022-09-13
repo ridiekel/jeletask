d3fcfaa15292f97a77534d30d88334645b7e60a9
SanderM2 <andy@knuts.be>
2022-09-13 21:11:55 +0200

    Update HARelayConfig.java
    Since 4.0.1 my relay switches and relay lights stopped working. 
    This fixed it.

--- jeletask2mqtt/src/main/java/io/github/ridiekel/jeletask/mqtt/listener/homeassistant/HARelayConfig.java
+++ jeletask2mqtt/src/main/java/io/github/ridiekel/jeletask/mqtt/listener/homeassistant/HARelayConfig.java
@@ -3,5 +3,15 @@ package io.github.ridiekel.jeletask.mqtt.listener.homeassistant;
 public class HARelayConfig extends HAReadWriteConfig<HARelayConfig> {
     public HARelayConfig(HAConfigParameters parameters) {
         super(parameters);
+        String type = parameters.getComponentSpec().getType();
+        switch ( type.toLowerCase() ) {
+            case "light":
+                this.put("state_value_template", "{{ value_json.state }}");
+                break;
+            case "switch":
+                this.put("value_template", "{{ value_json.state }}");
+                break;
+
+        }
     }
 }
