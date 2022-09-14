0d2fd0cc07fd114086db76a8791a5a863140de70
SanderM2 <andy@knuts.be>
2022-09-14 09:58:09 +0200

    Update GasStateCalculator.java

--- client/src/main/java/io/github/ridiekel/jeletask/client/builder/composer/config/statecalculator/GasStateCalculator.java
+++ client/src/main/java/io/github/ridiekel/jeletask/client/builder/composer/config/statecalculator/GasStateCalculator.java
@@ -8,25 +8,24 @@ public class GasStateCalculator extends SimpleStateCalculator {
         super(numberConverter);
     }
 
-    @Override
     public String convertGet(ComponentSpec component, byte[] value) {
         short base = this.getNumberConverter().convert(value).shortValue();
         float mMax = component.getGas_max();
         float mMin = component.getGas_min();
-        float value = 0;
+        float gas_value = 0;
         switch (component.getGas_type()) {
             case "4-20ma":
-                value = (mMax - mMin) / 704.0f * (base - 176) + mMin;
+                gas_value = (mMax - mMin) / 704.0f * (base - 176) + mMin;
                 break;
             case "0-10v":
             case "5-10v":
-                value = (mMax - mMin) / 1023.0f * base + mMin;
+                gas_value = (mMax - mMin) / 1023.0f * base + mMin;
                 break;
             case "0-20ma":
-                value = (mMax - mMin) / 880.0f * base + mMin;
+                gas_value = (mMax - mMin) / 880.0f * base + mMin;
                 break;
         }
 
-        return String.valueOf(value);
+        return String.valueOf(gas_value);
     }
 }
