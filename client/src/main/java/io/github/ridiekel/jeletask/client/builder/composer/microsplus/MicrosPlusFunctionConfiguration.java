bbd229def9c5623062b3f24fd5c57856d857bacd
SanderM2 <andy@knuts.be>
2022-09-27 12:56:36 +0200

    Update MicrosPlusFunctionConfiguration.java

--- client/src/main/java/io/github/ridiekel/jeletask/client/builder/composer/microsplus/MicrosPlusFunctionConfiguration.java
+++ client/src/main/java/io/github/ridiekel/jeletask/client/builder/composer/microsplus/MicrosPlusFunctionConfiguration.java
@@ -36,7 +36,7 @@ public class MicrosPlusFunctionConfiguration extends ConfigurationSupport<Functi
                         new GasStateCalculator(NumberConverter.UNSIGNED_SHORT)
                 )),
                 new FunctionConfigurable(Function.COND, 60, ON_OFF_TOGGLE),
-                new FunctionConfigurable(Function.SERVICE, 52, new ServiceStateCalculator(NumberConverter.UNSIGNED_SHORT)),
+                new FunctionConfigurable(Function.INPUT, 52, new ServiceStateCalculator(NumberConverter.UNSIGNED_SHORT)),
                 new FunctionConfigurable(Function.TIMEDFNC, 5, ON_OFF_TOGGLE)
         ));
     }
