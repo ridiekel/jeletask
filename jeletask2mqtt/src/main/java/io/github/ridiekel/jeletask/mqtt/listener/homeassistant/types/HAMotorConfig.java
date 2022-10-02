e094068bc4fb6b055696a8428a2aea4ff4137ae5
SanderM2 <andy@knuts.be>
2022-10-02 10:22:07 +0200

    Update HAMotorConfig.java

--- jeletask2mqtt/src/main/java/io/github/ridiekel/jeletask/mqtt/listener/homeassistant/types/HAMotorConfig.java
+++ jeletask2mqtt/src/main/java/io/github/ridiekel/jeletask/mqtt/listener/homeassistant/types/HAMotorConfig.java
@@ -19,6 +19,6 @@ public class HAMotorConfig extends HAReadWriteConfig<HAMotorConfig> {
         this.put("payload_stop", "STOP");
         this.put("state_stopped", "STOP");
         this.put("position_template", "{{ value_json.position }}");
-        this.put("value_template", "{% if value_json.state == 'OFF' %}STOP{% else %}value_json.last_direction{% endif %}");
+        this.put("value_template", "{% if value_json.state == 'OFF' %}STOP{% else %}{{value_json.last_direction}}{% endif %}");
     }
 }
