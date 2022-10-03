1c640d1e4f4eed6d4923be228f314024087176c9
SanderM2 <andy@knuts.be>
2022-10-03 08:49:18 +0200

    Update HASensorConfig.java

--- jeletask2mqtt/src/main/java/io/github/ridiekel/jeletask/mqtt/listener/homeassistant/types/HASensorConfig.java
+++ jeletask2mqtt/src/main/java/io/github/ridiekel/jeletask/mqtt/listener/homeassistant/types/HASensorConfig.java
@@ -20,7 +20,12 @@ public class HASensorConfig extends HAReadOnlyConfig<HASensorConfig> {
             this.put("temperature_state_topic", "~/state");
             this.put("temperature_command_topic", "~/set");
             this.put("power_state_topic", "~/state");
+
+            // If we don't enable power_command_topic HA won't send an ON command before a COOL/HEAT/DRY/... command.
+            // Teletask needs an ON command before any other mode is accepted.
+            // This will result in a double OFF command when turning off from HA but I don't think there's an easy solution to this right now?
             this.put("power_command_topic", "~/set");
+
             this.put("payload_on", "{\"state\": \"ON\"}");
             this.put("payload_off", "{\"state\": \"OFF\"}");
             this.put("mode_state_topic", "~/state");
@@ -31,7 +36,7 @@ public class HASensorConfig extends HAReadOnlyConfig<HASensorConfig> {
             this.put("temperature_state_template", "{{ value_json.target_temperature }}");
             this.put("temperature_command_template", "{\"target_temperature\": \"{{ value }}\"}");
             this.put("mode_state_template", "{% if value_json.mode|upper == \"VENT\" %}fan_only{% else %}{{ value_json.mode|lower }}{% endif %}");
-            this.put("mode_command_template", "{% if value|lower == \"fan_only\" %}{\"state\": \"VENT\"}{% else %}{\"state\": \"{{ value | lower}}\"}{% endif %}");
+            this.put("mode_command_template", "{% if value|lower == \"fan_only\" %}{\"state\": \"VENT\"}{% else %}{\"state\": \"{{ value | upper}}\"}{% endif %}");
             this.put("fan_mode_command_template", "{% if value|lower == \"auto\" %}{\"state\": \"SPAUTO\"}{% elif value|lower == \"low\" %}{\"state\": \"SPLOW\"}{% elif value|lower == \"med\" %}{\"state\": \"SPMED\"}{% elif value|lower == \"high\" %}{\"state\": \"SPHIGH\"}{% endif %}");
             this.put("fan_mode_state_template", "{% if value_json.fanspeed|upper == \"SPAUTO\" %}auto{% elif value_json.fanspeed|upper == \"SPLOW\" %}low{% elif value_json.fanspeed|upper == \"SPMED\" %}med{% elif value_json.fanspeed|upper == \"SPHIGH\" %}high{% endif %}");
 
