68d9984c0309d30006ecc3230b9fb06b9b625854
Kristof degrave <kristof.degrave@telenet.be>
2025-09-13 10:53:23 +0200

    Update HARelayConfig.java

--- jeletask2mqtt/src/main/java/io/github/ridiekel/jeletask/mqtt/listener/homeassistant/types/HARelayConfig.java
+++ jeletask2mqtt/src/main/java/io/github/ridiekel/jeletask/mqtt/listener/homeassistant/types/HARelayConfig.java
@@ -7,13 +7,16 @@ public class HARelayConfig extends HAReadWriteConfig<HARelayConfig> {
     public HARelayConfig(HAConfigParameters parameters) {
         super(parameters);
 
-        this.put("payload_on", "{\"state\": \"ON\"}");
-        this.put("payload_off", "{\"state\": \"OFF\"}");
-
         if ("light".equalsIgnoreCase(parameters.getComponentSpec().getType())) {
-
-            this.put("state_value_template", "{% if value_json.state|upper == \"ON\" %}{\"state\": \"ON\"}{% else %}{\"state\": \"OFF\"}{% endif %}");
+            // Code analog to dimmer implementation
+            this.put("schema", "template");
+            this.put("command_on_template", "{\"state\": \"on\"}");
+            this.put("command_off_template", "{\"state\": \"off\"}");
+            this.put("state_template", "{% if value_json.state|lower == 'on' %}on{% else %}off{% endif %}");
         } else {
+            this.put("payload_on", "{\"state\": \"ON\"}");
+            this.put("payload_off", "{\"state\": \"OFF\"}");
+
             this.put("state_value_template", "{{ value_json.state }}");
             this.put("state_on", "ON");
             this.put("state_off", "OFF");
