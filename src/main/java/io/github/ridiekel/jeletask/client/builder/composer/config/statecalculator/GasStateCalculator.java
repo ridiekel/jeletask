73b2ec14aa5c43a7e99096bc4e212377eaf2b3e5
ridiekel <geroen.dierckx@gmail.com>
2025-10-16 15:54:01 +0200

    Gas support

--- src/main/java/io/github/ridiekel/jeletask/client/builder/composer/config/statecalculator/GasStateCalculator.java
+++ src/main/java/io/github/ridiekel/jeletask/client/builder/composer/config/statecalculator/GasStateCalculator.java
@@ -3,9 +3,15 @@ package io.github.ridiekel.jeletask.client.builder.composer.config.statecalculat
 import io.github.ridiekel.jeletask.client.builder.composer.config.NumberConverter;
 import io.github.ridiekel.jeletask.client.spec.ComponentSpec;
 import io.github.ridiekel.jeletask.client.spec.state.impl.GasState;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.function.TriFunction;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * With Smax en Smin being the minimum and maximum values for “value”, the value can be calculated from the short as:
@@ -20,6 +26,57 @@ import java.math.RoundingMode;
  * Value = ( ( ( Smax - Smin) / 1023) x value ) + Smin
  */
 public class GasStateCalculator extends StateCalculatorSupport<GasState> {
+    private static final Map<String, GasTypeConfig> CONFIGS = Stream.of(
+            new GasTypeConfig(
+                    "4-20ma",
+                    (value, min, max) -> {
+                        return max.subtract(min)
+                                .divide(new BigDecimal("704.0"), 10, RoundingMode.HALF_UP)
+                                .multiply(value.subtract(new BigDecimal("176")))
+                                .add(min);
+                    },
+                    (value, min, max) -> {
+                        return max;
+                    }
+            ),
+            new GasTypeConfig(
+                    "0-10v",
+                    (value, min, max) -> {
+                        return max.subtract(min)
+                                .divide(new BigDecimal("1023.0"), 10, RoundingMode.HALF_UP)
+                                .multiply(value)
+                                .add(min);
+                    },
+                    (value, min, max) -> {
+                        return max;
+                    }
+            ),
+            new GasTypeConfig(
+                    "5-10v",
+                    (value, min, max) -> {
+                        return max.subtract(min)
+                                .divide(new BigDecimal("1023.0"), 10, RoundingMode.HALF_UP)
+                                .multiply(value)
+                                .add(min);
+                    },
+                    (value, min, max) -> {
+                        return max;
+                    }
+            ),
+            new GasTypeConfig(
+                    "0-20ma",
+                    (value, min, max) -> {
+                        return max.subtract(min)
+                                .divide(new BigDecimal("880.0"), 10, RoundingMode.HALF_UP)
+                                .multiply(value)
+                                .add(min);
+                    },
+                    (value, min, max) -> {
+                        return max;
+                    }
+            )
+    ).collect(Collectors.toMap(t -> t.name, Function.identity()));
+
     @Override
     public GasState fromEvent(ComponentSpec component, byte[] dataBytes) {
         // Default value
@@ -31,8 +88,8 @@ public class GasStateCalculator extends StateCalculatorSupport<GasState> {
 
         // There are 3 different General Analog Sensors in Teletask.
         gas_value = switch (component.getGas_type().toLowerCase()) {
-            case "4-20ma" -> (mMax - mMin) / 704.0f * (gas_value - 176) + mMin;
-            case "0-10v", "5-10v" -> (mMax - mMin) / 1023.0f * gas_value + mMin;
+            case "4-20ma" -> fromOne(gas_value, mMin, mMax);
+            case "0-10v", "5-10v" -> fromTwo(gas_value, mMin, mMax);
             case "0-20ma" -> (mMax - mMin) / 880.0f * gas_value + mMin;
             default -> gas_value;
         };
@@ -44,13 +101,39 @@ public class GasStateCalculator extends StateCalculatorSupport<GasState> {
         return new GasState(gas_rounded_value);
     }
 
+    private static float fromThree(float gas_value, float mMin, float mMax) {
+        return (mMax - mMin) / 880.0f * gas_value + mMin;
+    }
+
+    private static float fromTwo(float gas_value, float mMin, float mMax) {
+        return (mMax - mMin) / 1023.0f * gas_value + mMin;
+    }
+
+    private static float fromOne(float gas_value, float mMin, float mMax) {
+        return (mMax - mMin) / 704.0f * (gas_value - 176) + mMin;
+    }
+
     @Override
     public byte[] toCommand(GasState state) {
         return new byte[]{0, 0};
     }
 
     @Override
+    public GasState fromCommandForTesting(ComponentSpec component, byte[] dataBytes) {
+        return super.fromCommandForTesting(component, dataBytes);
+    }
+
+    @Override
     protected Class<GasState> getStateType() {
         return GasState.class;
     }
+
+    @RequiredArgsConstructor
+    private static final class GasTypeConfig {
+
+        private final String name;
+        private final TriFunction<BigDecimal, BigDecimal, BigDecimal, BigDecimal> fromEvent;
+        private final TriFunction<BigDecimal, BigDecimal, BigDecimal, BigDecimal> toCommand;
+    }
+
 }
